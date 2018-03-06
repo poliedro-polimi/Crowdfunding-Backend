4d3d5e670a26a111802fd2ab138c0d9c1f458d33
Davide Depau <davide@depau.eu>
2018-03-06 15:55:03 +0100

    Fix typo

--- poliedro_donate/config.py
+++ poliedro_donate/config.py
@@ -5,8 +5,8 @@ class DefaultConfig(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
     PAYPAL_CLIENT_ID = ""
     PAYPAL_CLIENT_SECRET = ""
-    PAYPAL_API_URL = paypalrestsdk.core.PayPalEnvironment.SANDBOX_API_URL,
-    PAYPAL_WEB_URL = paypalrestsdk.core.PayPalEnvironment.SANDBOX_WEB_URL,
+    PAYPAL_API_URL = paypalrestsdk.core.PayPalEnvironment.SANDBOX_API_URL
+    PAYPAL_WEB_URL = paypalrestsdk.core.PayPalEnvironment.SANDBOX_WEB_URL
     APP_DOMAIN = "poliedropolimi.pythonanywhere.com"
     APP_SSL = True
     APP_WEB_ROOT = ""
