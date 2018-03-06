e6e530a6905867bf1541a999173060a77ef39c6f
Davide Depau <davide@depau.eu>
2018-03-06 14:33:35 +0100

    Remove useless config

--- poliedro_donate/config.py
+++ poliedro_donate/config.py
@@ -2,9 +2,6 @@ import paypalrestsdk.core
 
 
 class DefaultConfig(object):
-    SUPPORTED_LANGUAGES =  {'it': 'Italiano', 'en': 'English'}
-    BABEL_DEFAULT_LOCALE = 'it'
-    BABEL_DEFAULT_TIMEZONE = 'Europe/Rome'
     SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
     PAYPAL_CLIENT_ID = ""
     PAYPAL_CLIENT_SECRET = ""
