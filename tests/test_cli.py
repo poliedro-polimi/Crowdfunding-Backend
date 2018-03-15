05ec3d4379cb70a6d9e20a74e0eebf81de0651f1
Davide Depau <davide@depau.eu>
2018-03-15 18:37:04 +0100

    Fix CLI tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,22 @@
+import os
+import pytest
+
 from poliedro_donate.cli import *
 
 
+@pytest.fixture(scope="module", autouse=True)
+def setup_app_env():
+    os.environ["FLASK_APP"] = "poliedro_donate"
+    yield
+
+
 def test_initdb():
-    initdb()
+    with pytest.raises(SystemExit) as exc:
+        initdb()
+    assert exc.value.args[0] == 0
 
 
 def test_cleardb():
-    cleardb()
+    with pytest.raises(SystemExit) as exc:
+        cleardb()
+    assert exc.value.args[0] == 0
