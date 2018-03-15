9dce7b4c2f11382888ef4080a73786d4f41ecd81
Davide Depau <davide@depau.eu>
2018-03-15 18:20:29 +0100

    Remove useless module

--- poliedro_donate/utils.py
+++
@@ -1,6 +0,0 @@
-from poliedro_donate import app
-
-
-def get_base_url():
-    return (app.config["APP_SSL"] and "https://" or "http://") + \
-           app.config["APP_DOMAIN"] + app.config["APP_WEB_ROOT"]
