cfdf2c3cb281e4359ba8a91d35122f823a253a44
Davide Depau <davide@depau.eu>
2018-03-31 17:42:12 +0200

    Also include transaction and donation ID in response to client

--- poliedro_donate/errors.py
+++ poliedro_donate/errors.py
@@ -11,8 +11,12 @@ class DonationError(Exception):
     def __init__(self, donation, parent_exc=None):
         self.donation = donation
         self.parent_exc = parent_exc
+        self.donation_id = "#D{}-T{}".format(donation.id, donation.transaction.id)
 
-        super(DonationError, self).__init__("Donation ID: #D{}-T{}".format(donation.id, donation.transaction.id))
+        if parent_exc:
+            parent_exc.donation_id = self.donation_id
+
+        super(DonationError, self).__init__("Donation ID: {}".format(self.donation_id))
 
 
 @app.errorhandler(braintreehttp.HttpError)
@@ -33,16 +37,24 @@ def handle_paypal_error(error):
     jerr = json.loads(error.message)
 
     if error.status_code == 403:
-        response = jsonify({"error": {
-            "type": jerr["name"],
-            "message": jerr["message"]
-        }})
-        response.status_code = 403
+        ejson = {"error": {
+                "type": jerr["name"],
+                "message": jerr["message"]
+            }}
     else:
-        response = jsonify({"error": {
+        ejson = {"error": {
             "type": "_PAYPAL_ERROR",
             "message": "There was an error processing the payment. This error has been logged and we'll try to fix it as soon as possible. In the meantime, make sure your data is correct. Please submit an issue at https://github.com/poliedro-polimi/Crowdfunding-Backend/issues"
-        }})
+        }}
+
+    if getattr(error, "donation_id", None):
+        ejson["error"]["donation_id"] = error.donation_id
+
+    response = jsonify(ejson)
+
+    if error.status_code == 403:
+        response.status_code = 403
+    else:
         # https://pics.me.me/502-bad-gateway-nginx-0-7-67-502-bad-gateway-4364222.png
         response.status_code = 502
 
@@ -70,6 +82,9 @@ def handle_invalid_usage(error):
     if getattr(error, "edesc", None):
         ejson["error"]["desc"] = error.edesc
 
+    if getattr(error, "donation_id", None):
+        ejson["error"]["donation_id"] = error.donation_id
+
     response = jsonify(ejson)
     response.status_code = 400
     return response
@@ -78,9 +93,14 @@ def handle_invalid_usage(error):
 @app.errorhandler(Exception)
 def handle_generic_exception(error):
     traceback.print_exc(file=sys.stderr)
-    response = jsonify({"error": {
+    ejson = {"error": {
         "type": "_APP_ERROR",
         "message": "500 Internal Server Error"}
-    })
+    }
+
+    if getattr(error, "donation_id", None):
+        ejson["error"]["donation_id"] = error.donation_id
+
+    response = jsonify(ejson)
     response.status_code = 500
     return response
