e7fbc0195a60827ac1ac2bcfa5873a779d1a6082
Davide Depau <davide@depau.eu>
2018-03-31 14:46:05 +0200

    Use custom BrainTreeHTTP module instead of PyPI version

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 
+test_deps = ['pytest', 'pytest-runner', 'pytest-flask']
+
 setup(
     name='PoliEdro Donation Website',
     version='0.1',
@@ -7,11 +9,11 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=['Flask', 'Flask-SQLAlchemy', 'paypalrestsdk==2.0.0rc1', 'flask-cors'],
+    install_requires=['Flask', 'Flask-SQLAlchemy', 'paypalrestsdk==2.0.0rc1', 'flask-cors'] + test_deps,
     dependency_links=[
-        "https://github.com/paypal/PayPal-Python-SDK/archive/2.0-beta.zip"
+        "https://github.com/Depau/braintreehttp_python-noparseresponse/archive/noparse.zip#egg=braintreehttp-0.4.3"
     ],
-    setup_requires=['pytest', 'pytest-runner', 'pytest-flask'],
+    test_require=test_deps,
     entry_points={
         'console_scripts': [
             'poliedro_donate = poliedro_donate.cli:main',
