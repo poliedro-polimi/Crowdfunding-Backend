97bede49c9cba3d921864f347f3d42cfd0d5bacc
Davide Depau <davide@depau.eu>
2018-04-01 21:49:22 +0200

    Add admin user management CLI and tests

--- poliedro_donate/cli.py
+++ poliedro_donate/cli.py
@@ -1,5 +1,7 @@
-import click
+import click, getpass
 from socket import gethostname
+
+from poliedro_donate.database.models import AdminUser
 
 from . import app
 from .database import db, commit_on_success
@@ -30,3 +32,49 @@ def cleardb():
     click.echo('Dropping database tables')
     db.reflect()
     db.drop_all()
+
+
+@app.cli.command()
+@click.argument("username")
+@click.argument("firstname", required=False)
+@click.argument("lastname", required=False)
+@click.argument("password", required=False)
+@commit_on_success
+def adduser(username, firstname=None, lastname=None, password=None):
+    """Create new admin user."""
+    if not password:
+        password = getpass.getpass('Password: ')
+    u = AdminUser(username, password, firstname, lastname)
+    db.session.add(u)
+
+
+@app.cli.command()
+@click.argument("username")
+@commit_on_success
+def deluser(username):
+    """Delete admin user."""
+    q = AdminUser.query.filter_by(username=username)
+
+    if q.count() <= 0:
+        raise ValueError("User does not exist")
+
+    u = q[0]
+
+    db.session.delete(u)
+
+
+@app.cli.command()
+@click.argument("username")
+@click.argument("password", required=False)
+@commit_on_success
+def passwd(username, password=None):
+    """Change admin user password."""
+    if not password:
+        password = getpass.getpass('New password: ')
+    q = AdminUser.query.filter_by(username=username)
+
+    if q.count() <= 0:
+        raise ValueError("User does not exist")
+
+    u = q[0]
+    u.set_password(password)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -20,3 +20,53 @@ def test_cleardb():
     with pytest.raises(SystemExit) as exc:
         cleardb()
     assert exc.value.args[0] == 0
+
+
+def test_add_del_user():
+    db.drop_all()
+    db.session.commit()
+    db.create_all()
+    db.session.commit()
+    with pytest.raises(SystemExit) as exc:
+        adduser(['test', 'Test', 'User', 'password'])
+    assert exc.value.args[0] == 0
+
+    u = AdminUser.query.filter_by(username='test')
+    assert u.count() == 1
+    assert u[0].check_password('password')
+
+    with pytest.raises(SystemExit) as exc:
+        deluser(['test'])
+    assert exc.value.args[0] == 0
+
+    u = AdminUser.query.filter_by(username='test')
+    assert u.count() == 0
+    db.drop_all()
+    db.session.commit()
+
+
+def test_user_chpasswd():
+    db.drop_all()
+    db.session.commit()
+    db.create_all()
+    db.session.commit()
+    with pytest.raises(SystemExit) as exc:
+        adduser(['test', 'Test', 'User', 'password'])
+    assert exc.value.args[0] == 0
+
+    with pytest.raises(SystemExit) as exc:
+        passwd(['test', 'newpassword'])
+    assert exc.value.args[0] == 0
+
+    u = AdminUser.query.filter_by(username='test')
+    assert u.count() == 1
+    assert u[0].check_password('newpassword')
+
+    with pytest.raises(SystemExit) as exc:
+        deluser(['test'])
+    assert exc.value.args[0] == 0
+
+    u = AdminUser.query.filter_by(username='test')
+    assert u.count() == 0
+    db.drop_all()
+    db.session.commit()
